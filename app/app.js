611b871362774179e6b0f5ff6928f6ae38a1297d
Jakucs Viktor <jakucs42@gmail.com>
2024-11-20 21:03:39 +0100

    Házifeladat kész

--- app/app.js
+++ app/app.js
@@ -11,6 +11,6 @@ objektum.szamitasGomb.addEventListener('click', () => {
     var alfaSzogRadianja = alfaSzog * (Math.PI / 180);
     var alfaSzogSzinusza = Math.sin(alfaSzogRadianja);
     var rombuszbaIrhatoSugar = (1 / 2) * (oldalhossz * alfaSzogSzinusza);
-    objektum.h1.innerHTML = '<p>' + "Kis pontatlansággal (lebegőpontos számok természetéből fakadóan) a végeredmény, a rombuszba írható kör sugara: " + '</p>' + '<h1>' + rombuszbaIrhatoSugar + '</h1>' + '<p>' + 'Mivel azt nem tetszett írni, hogy OOP alapelven vagy esetleg nélküle kell megoldani a feladatot megoldottam OOPvel is illetve outputnak inputot használtam (bár szerintem szebb a h1 tag kimenetnek)' + '</p>';
+    objektum.h1.innerHTML = '<p>' + "Kis pontatlansággal (lebegőpontos számok természetéből fakadóan) a végeredmény, a rombuszba írható kör sugara: " + '</p>' + '<h1>' + rombuszbaIrhatoSugar + '</h1>' + '<p>' + 'Mivel azt nem tetszett írni, hogy OOP alapelven vagy esetleg nélküle kell megoldani a feladatot megoldottam OOPvel is illetve outputnak inputot használtam (bár szerintem szebb a h1 tag kimenetnek)' + '</p>' + '<p>' + '<br><br><br>' + 'OOP -s megoldás link: ' + '</p>' + '<p>' + 'https://github.com/Jakucs/hazifeladat3.git' + '</p>';
     console.log(rombuszbaIrhatoSugar);
 });
